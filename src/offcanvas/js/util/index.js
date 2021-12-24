a862a3681b2b9936b49b5116e31718f1753c5b95
zhangchenglin <469946668@qq.com>
2021-12-25 00:32:48 +0800

    Initial commit

--- src/offcanvas/js/util/index.js
+++ src/offcanvas/js/util/index.js
@@ -2,6 +2,17 @@ const getTimeString = () => {
   return new Date().getTime().toString()
 }
 
+function removeOffcanvas(offcanvas_id) {
+  const offcanvas_element = document.querySelector("#" + offcanvas_id);
+  offcanvas_element.addEventListener("hidden.bs.offcanvas", function () {
+    let x = bootstrap.Offcanvas.getInstance(offcanvas_element);
+    x.dispose();
+    setTimeout(function () {
+      offcanvas_element.parentElement.removeChild(offcanvas_element);
+    }, 3e3);
+  });
+}
+
 
 /**
  * @param {string} offcanvasId
@@ -12,22 +23,22 @@ const offcanvasEvents = (offcanvasId, eventName, eventFunction) => {
   const offcanvas = document.querySelector("#Offcanvas_" + offcanvasId);
   switch (eventName) {
     case "show":
-      offcanvas.addEventListener("show.bs.modal", function () {
+      offcanvas.addEventListener("show.bs.offcanvas", function () {
         return eventFunction();
       });
       break;
     case "shown":
-      offcanvas.addEventListener("shown.bs.modal", function () {
+      offcanvas.addEventListener("shown.bs.offcanvas", function () {
         return eventFunction();
       });
       break;
     case "hide":
-      offcanvas.addEventListener("hide.bs.modal", function () {
+      offcanvas.addEventListener("hide.bs.offcanvas", function () {
         return eventFunction();
       });
       break;
     case "hidden":
-      offcanvas.addEventListener("hidden.bs.modal", function () {
+      offcanvas.addEventListener("hidden.bs.offcanvas", function () {
         return eventFunction();
       });
       break;
