5903d6dd5a863c8d23fc8d69c3d6c48788c68075
zhangchenglin <469946668@qq.com>
2022-03-26 17:06:57 +0800

    0.0.3

--- src/bundle/index.esm.js
+++ src/bundle/index.esm.js
@@ -1,3 +1,4 @@
 export { default as bModal } from '../modal/js/bModal'
 export { default as bOffcanvas } from '../offcanvas/js/bOffcanvas'
 export { default as bToast } from '../toast/js/bToast'
+export { default as bTooltip } from '../tooltip/js/bTooltip'
--- src/bundle/index.umd.js
+++ src/bundle/index.umd.js
@@ -1,9 +1,11 @@
 import bModal from '../modal/js/bModal'
 import bOffcanvas from '../offcanvas/js/bOffcanvas'
 import bToast from '../toast/js/bToast'
+import bTooltip from '../tooltip/js/bTooltip'
 
 Object.assign(window, {
   bModal,
   bOffcanvas,
-  bToast
+  bToast,
+  bTooltip
 })
